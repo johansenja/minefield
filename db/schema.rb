b5e41dedf36210104de8fc2b3956d95a1dc940c2
Joseph Johansen <jo.johansen@hotmail.com>
2018-11-11 19:07:24 +0000

    changed database.yml file

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_11_11_142058) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "scores", force: :cascade do |t|
     t.string "name"
     t.integer "score"
